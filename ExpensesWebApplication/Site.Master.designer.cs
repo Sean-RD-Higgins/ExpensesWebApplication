a0e326538ff875678f5d6c523baa274676fec1e3
Sean-RD-Higgins <sean.rd.higgins@gmail.com>
2024-03-10 19:18:35 -0400

    Moved all CSS and JS scripts to their own files.

--- ExpensesWebApplication/Site.Master.designer.cs
+++ ExpensesWebApplication/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace ExpensesWebApplication
     {
 
         /// <summary>
+        /// ScriptContentPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ScriptContentPlaceHolder;
+
+        /// <summary>
         /// mainForm control.
         /// </summary>
         /// <remarks>
